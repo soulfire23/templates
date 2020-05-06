3cb890b27742235e701d1b774e910665c85ef53c
Anton Z <its.for.regs@gmail.com>
2020-05-06 14:58:50 +0400

    update gulp file

--- default-php-js/gulpfile.js
+++ default-php-js/gulpfile.js
@@ -1,6 +1,15 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+sass.compiler = require('node-sass');
 
+// Imagemin
 exports.imagemin = () => (
   gulp.src('src/images/**')
     .pipe(imagemin([
@@ -16,3 +25,33 @@ exports.imagemin = () => (
     ]))
     .pipe(gulp.dest('dist/images'))
 );
+
+// Sass
+gulp.task('sass', () => {
+  return gulp.src('src/sass/main.scss')
+    .pipe(sourcemaps.init())
+    //  .pipe(concat('style.css'))
+    .pipe(rename('style.min.css'))
+    .pipe(sass().on('error', sass.logError))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/assets/css'));
+});
+
+gulp.task('sass:watch', () => {
+  gulp.watch('src/sass/**/*.scss', gulp.series('sass'));
+});
+
+// Babel
+gulp.task('babel', (done) => {
+  gulp.src('src/js/script.js')
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['@babel/env'],
+    }))
+    .pipe(rename('script.min.js'))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/assets/js/'));
+  done();
+});
